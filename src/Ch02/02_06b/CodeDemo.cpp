279482689e4f3f7b1d2139778352b6645df549b0
Soumadip Dey <souma.hitech@gmail.com>
2024-05-10 07:34:37 +0000

    Learned conditional compilation

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,20 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <cstdint>
+#define CAPACITY 5000
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 10;
+    #ifdef DEBUG
+    std::cout<<"[About to perform the addition]"<<std::endl;
+    #endif
+
+    large += small;
+    std::cout<<"Large number: "<< large << std::endl;
+  
     std::cout << std::endl << std::endl;
     return (0);
 }
